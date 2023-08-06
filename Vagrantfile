6b26b12a535db01608a726269a73063c11667de4
tomekbielaszewski <tomekbielaszewski@gmail.com>
2023-08-06 23:01:42 +0200

    5 node nomad lab with vagrant

---
+++ Vagrantfile
@@ -0,0 +1,24 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "bento/ubuntu-22.04" # 22.04 LTS, Jammy
+  config.vm.provider "virtualbox" do |vb|
+    vb.memory = 2048
+    vb.cpus = 4
+  end
+
+  # 5-node configuration
+  (1..5).each do |i|
+    config.vm.define "nomad-node-#{i}" do |n|
+      n.vm.provision "shell", path: "node-install.sh"
+      if i == 1
+        # Expose the nomad ports
+        n.vm.network "forwarded_port", guest: 4646, host: 4646, auto_correct: true
+      end
+      n.vm.hostname = "nomad-node-#{i}"
+      n.vm.network "private_network", ip: "172.16.1.#{i+100}"
+    end
+  end
+end
---
+++ node-install.sh
@@ -0,0 +1,63 @@
+echo "Installing Docker..."
+sudo apt-get update
+sudo apt-get remove docker docker-engine docker.io
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+      stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+# Restart docker to make sure we get the latest version of the daemon if there is an upgrade
+sudo service docker restart
+# Make sure we can actually use docker as the vagrant user
+sudo usermod -aG docker vagrant
+sudo docker --version
+
+# Packages required for nomad & consul
+sudo apt-get install unzip curl vim -y
+
+echo "Installing Nomad..."
+NOMAD_VERSION=1.3.1
+cd /tmp/
+curl -sSL https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip -o nomad.zip
+unzip nomad.zip
+sudo install nomad /usr/bin/nomad
+sudo mkdir -p /etc/nomad.d
+sudo chmod a+w /etc/nomad.d
+
+
+echo "Installing Consul..."
+CONSUL_VERSION=1.12.2
+curl -sSL https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip > consul.zip
+unzip /tmp/consul.zip
+sudo install consul /usr/bin/consul
+(
+cat <<-EOF
+  [Unit]
+  Description=consul agent
+  Requires=network-online.target
+  After=network-online.target
+
+  [Service]
+  Restart=on-failure
+  ExecStart=/usr/bin/consul agent -dev
+  ExecReload=/bin/kill -HUP $MAINPID
+
+  [Install]
+  WantedBy=multi-user.target
+EOF
+) | sudo tee /etc/systemd/system/consul.service
+sudo systemctl enable consul.service
+sudo systemctl start consul
+
+for bin in cfssl cfssl-certinfo cfssljson
+do
+  echo "Installing $bin..."
+  curl -sSL https://pkg.cfssl.org/R1.2/${bin}_linux-amd64 > /tmp/${bin}
+  sudo install /tmp/${bin} /usr/local/bin/${bin}
+done
+nomad -autocomplete-install
